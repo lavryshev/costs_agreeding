e82ef41c588be5060485566abfb80ede6aa6e4b2
Kostya <lavryshev@gmail.com>
2023-05-11 14:20:41 +0000

    Add bank account's controller's methods and bank account's views'

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -1,19 +1,48 @@
 class BankAccountsController < ApplicationController
+  before_action :set_bank_account, only: [:edit, :update, :destroy]
+
   def index
+    @bank_accounts = BankAccount.all
   end
 
   def new
+    @bank_account = BankAccount.new
   end
 
   def create
+    @bank_account = BankAccount.create(bank_account_params)
+
+    if @bank_account.save
+      redirect_to bank_accounts_path, notice: "Банковский счет создан успешно."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @bank_account.update(bank_account_params)
+      redirect_to bank_accounts_path, notice: "Банковский счет изменен успешно."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @bank_account.destroy
+    redirect_to bank_accounts_path, notice: "Банковский счет удален."
   end
+
+  private
+
+  def bank_account_params
+    params.require(:bank_account).permit(:name)
+  end
+
+  def set_bank_account
+    @bank_account = BankAccount.find(params[:id])
+  end
+
 end
