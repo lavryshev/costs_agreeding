9c75e819d257eeacb8c69d750de8666451d93ddc
Kostya <lavryshev@gmail.com>
2023-05-11 12:03:10 +0000

    Add controllers Add navigation partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :bank_accounts
+  resources :cashboxes
+  resources :users
+  resources :expenses
+  root "pages#home"
 end
