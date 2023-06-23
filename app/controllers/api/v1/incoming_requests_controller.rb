1f458cf714eba19c720aca8e0ccd9e9de2aea97e
Kostya <lavryshev@gmail.com>
2023-06-23 16:46:44 +0000

    Added inserting incoming request to the database

--- app/controllers/api/v1/incoming_requests_controller.rb
+++ app/controllers/api/v1/incoming_requests_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::IncomingRequestsController < Api::V1::AuthenticatedController
 
   def create_expense
-    render json: { message: 'Create expense' }
+    @incoming_request = IncomingRequest.create(api_user: current_api_user, action: 'create_expense', data: request.body.read)
+    if @incoming_request.save
+      render json: {message: 'Success'}, status: :ok
+    else
+      render json: {message: 'Error'}, status: :internal_server_error
+    end
   end
 
 end
