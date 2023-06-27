0ae098e489c322d8d563929754e6eadb0f7d4536
Kostya <lavryshev@gmail.com>
2023-06-27 22:20:00 +0000

    Added ProcessIncomingRequestJob testing. Fixed API incoming request testing.

--- test/integration/api/incoming_requests_controller_test.rb
+++ test/integration/api/incoming_requests_controller_test.rb
@@ -1,19 +1,25 @@
 require 'test_helper'
 
 class IncomingRequestsControllerTest < ActionDispatch::IntegrationTest
-  test "should create expense" do
+  test "POST /api/v1/addexpense should save incoming request and add enqued job" do
+    assert_enqueued_jobs 0
+    
     api_user = api_users(:active)
-    post api_v1_addexpense_path, headers: { HTTP_AUTHORIZATION: "Token token=#{api_user.token}" }
+    post api_v1_addexpense_path, headers: { HTTP_AUTHORIZATION: "Token token=#{api_user.token}", 'Content-Type' => 'application/json' }, params: {sum: 100}, as: :json
+    
+    assert_enqueued_jobs 1
+    
     assert_response :success
+
   end
 
-  test "with invalid token shouldn't create expense" do
+  test "POST /api/v1/addexpense with invalid token shouldn't save incoming request" do
     api_user = api_users(:active)
     post api_v1_addexpense_path, headers: { HTTP_AUTHORIZATION: "Token token=bad" }
     assert_response :unauthorized
   end
 
-  test "not active API user shouldn't create expense" do
+  test "POST /api/v1/addexpense not active API user shouldn't save incoming request" do
     api_user = api_users(:not_active)
     post api_v1_addexpense_path, headers: { HTTP_AUTHORIZATION: "Token token=#{api_user.token}" }
     assert_response :unauthorized
--- test/jobs/process_incoming_request_job_test.rb
+++ test/jobs/process_incoming_request_job_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class ProcessIncomingRequestJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should process incoming request" do
+    cashbox = cashboxes(:uah)
+    author = users(:user1)
+    data = {'source_type': 'Cashbox', 'source_id': cashbox.id, 'sum': 100, author_id: author.id}
+    r = IncomingRequest.create(api_user: api_users(:active), action: 'create_expense', data: data)
+
+    assert_difference("IncomingRequest.count", -1) do
+      ProcessIncomingRequestJob.perform_now
+    end
+  end
 end
