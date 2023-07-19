c4d6b0d2991027737453052a190a23a938c89798
Igor Alexandrov <igor.alexandrov@gmail.com>
2023-07-19 11:42:03 +0400

    Update expense_status.rb
    Added ExpenseStatus scopes

--- app/models/expense_status.rb
+++ app/models/expense_status.rb
@@ -1,17 +1,21 @@
 class ExpenseStatus < ApplicationRecord
-  class << self
-    def not_agreed
-      ExpenseStatus.find(1)
-    end
+  # class << self
+  #   def not_agreed
+  #     ExpenseStatus.find(1)
+  #   end
 
-    def agreed
-      ExpenseStatus.find(2)
-    end
+  #   def agreed
+  #     ExpenseStatus.find(2)
+  #   end
 
-    def rejected
-      ExpenseStatus.find(3)
-    end
-  end
+  #   def rejected
+  #     ExpenseStatus.find(3)
+  #   end
+  # end
+
+  scope :not_agreed, -> { find(1) }
+  scope :agreed, -> { find(2) }
+  scope :rejected, -> { find(3) }
 
   validates :name, presence: true
 end
