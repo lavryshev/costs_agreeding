6ae882b751052c47b508e87a9623f25a3332e4e4
Kostya <lavryshev@gmail.com>
2023-08-12 18:35:16 +0000

    Added create, update and destroy actions to the expenses controller

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       defaults format: :json do
-        post '/addexpense', to: 'expenses#create'
+        resources :expenses, only: [:create, :update, :destroy]
       end
     end
   end
