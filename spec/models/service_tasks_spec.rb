c2f1472b94ea4474bcaa9c6b6f6c852254003c7e
Kostya <lavryshev@gmail.com>
2023-08-20 18:28:23 +0000

    Removed model spec

--- spec/models/service_tasks_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ServiceTask, type: :model do
-  it { is_expected.to validate_presence_of(:action) }
-  it { is_expected.to validate_presence_of(:data) }
-  it { is_expected.to validate_presence_of(:external_app) }
-end
